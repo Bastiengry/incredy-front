9ff9d0ca154558b458a2ca5561538172861beb85
bgry <bgryspee@gmail.com>
2024-11-03 17:24:14 +0100

    Add unit tests

--- src/pages/__tests__/EditTopic.test.tsx
+++ src/pages/__tests__/EditTopic.test.tsx
@@ -1,17 +1,33 @@
 import '../../../public/appConfig';
-import {http, HttpResponse} from 'msw';
+import {DefaultBodyType, http, HttpResponse, StrictRequest} from 'msw';
 import {setupServer} from 'msw/node';
-import {render, screen, waitFor, within} from '@testing-library/react';
+import {render, screen, fireEvent, waitFor} from '@testing-library/react';
 import {createMemoryRouter, RouterProvider} from 'react-router-dom';
 import EditTopic from '../EditTopic';
 import {EditorProps} from 'primereact/editor';
 import {useParams} from 'react-router-dom';
+import {Api} from '../../api';
 
 const mockEditor = jest.fn();
 jest.mock('primereact/editor', () => ({
-  Editor: ({value}: EditorProps) => {
+  Editor: ({value, onTextChange}: EditorProps) => {
     mockEditor(({value}: any) => ({value}));
-    return <>{value}</>;
+    return (
+      <input
+        type="text"
+        aria-label="mock-editor"
+        value={value || ''}
+        onChange={e =>
+          onTextChange &&
+          onTextChange({
+            htmlValue: e?.target.value || '',
+            textValue: e?.target.value || '',
+            delta: undefined,
+            source: '',
+          })
+        }
+      />
+    );
   },
 }));
 
@@ -59,11 +75,7 @@ const dataValueTopic1 = {
   status: 'SUCCESS',
 };
 
-const server = setupServer(
-  http.get('/incredy/api/v1/topic/1', () => {
-    return HttpResponse.json(dataValueTopic1);
-  }),
-);
+const server = setupServer();
 
 beforeAll(() => {
   global.TextEncoder = require('util').TextEncoder;
@@ -78,18 +90,250 @@ describe('The edit topic component', () => {
   });
 
   it('should display empty editor if open in add mode', async () => {
-    useParams.mockReturnValue({});
+    useParams.mockReturnValue({topicId: 'add'});
+
     const router = createMemoryRouter([{path: '*', element: <EditTopic />}]);
     render(<RouterProvider router={router} />);
-    const editor = await screen.findByLabelText('editor');
-    expect(editor).toHaveTextContent('');
+    const titleElement = await screen.findByLabelText('title');
+    expect(titleElement).toHaveValue('');
+    const editorElement = await screen.findByLabelText('mock-editor');
+    expect(editorElement).toHaveValue('');
   });
 
   it('should display the topic when editing one', async () => {
     useParams.mockReturnValue({topicId: '1'});
+
+    const myGetHandler = http.get(
+      APP_CONFIG.BACKEND_PREFIX + Api.Topic.update(1),
+      () => {
+        return HttpResponse.json(dataValueTopic1);
+      },
+    );
+    server.use(myGetHandler);
+
     const router = createMemoryRouter([{path: '*', element: <EditTopic />}]);
     render(<RouterProvider router={router} />);
-    const editor = await screen.findByLabelText('editor');
-    expect(editor).toHaveTextContent(dataValueTopic1.data.text);
+    const titleElement = await screen.findByLabelText('title');
+    expect(titleElement).toHaveValue(dataValueTopic1.data.title);
+    const editorElement = await screen.findByLabelText('mock-editor');
+    expect(editorElement).toHaveValue(dataValueTopic1.data.text);
+  });
+
+  it('should be able to save changes in add mode', async () => {
+    useParams.mockReturnValue({topicId: 'add'});
+
+    let postRequestObject: StrictRequest<DefaultBodyType>;
+    let postRequestData: any;
+    const myPostHandler = http.post(
+      APP_CONFIG.BACKEND_PREFIX + Api.Topic.create(),
+      async ({request, params}) => {
+        postRequestObject = request;
+        postRequestData = await request.clone().json();
+        return HttpResponse.json({});
+      },
+    );
+    server.use(myPostHandler);
+
+    const router = createMemoryRouter([{path: '*', element: <EditTopic />}]);
+    render(<RouterProvider router={router} />);
+
+    // Gets the elements
+    const titleElement = await screen.findByLabelText('title');
+    expect(titleElement).toHaveValue('');
+    const editorElement = await screen.findByLabelText('mock-editor');
+    expect(editorElement).toHaveValue('');
+
+    // Set data
+    fireEvent.change(titleElement, {target: {value: 'new title 1'}});
+    expect(titleElement).toHaveValue('new title 1');
+    fireEvent.change(editorElement, {target: {value: 'new content 1'}});
+    expect(editorElement).toHaveValue('new content 1');
+
+    // Click on submit button
+    const submitButton = await waitFor(async () =>
+      screen.findByRole('button', {
+        name: 'submit',
+      }),
+    );
+    await waitFor(async () => submitButton.click());
+
+    // Check API call
+    await waitFor(() =>
+      expect(postRequestObject).toEqual(
+        expect.objectContaining({
+          method: 'POST',
+          url: 'http://localhost/incredy/api/v1/topic',
+        }),
+      ),
+    );
+
+    await waitFor(() =>
+      expect(postRequestData).toEqual(
+        expect.objectContaining({
+          title: 'new title 1',
+          text: 'new content 1',
+        }),
+      ),
+    );
+  });
+
+  it('should be able to save changes in edit mode', async () => {
+    useParams.mockReturnValue({topicId: '1'});
+
+    const myGetHandler = http.get(
+      APP_CONFIG.BACKEND_PREFIX + Api.Topic.get(1),
+      () => {
+        return HttpResponse.json(dataValueTopic1);
+      },
+    );
+    server.use(myGetHandler);
+
+    let putRequestObject: StrictRequest<DefaultBodyType>;
+    let putRequestData: any;
+    const myPutHandler = http.put(
+      APP_CONFIG.BACKEND_PREFIX + Api.Topic.update(1),
+      async ({request, params}) => {
+        putRequestObject = request;
+        putRequestData = await request.clone().json();
+        return HttpResponse.json({});
+      },
+    );
+    server.use(myPutHandler);
+
+    const router = createMemoryRouter([{path: '*', element: <EditTopic />}]);
+    render(<RouterProvider router={router} />);
+
+    // Gets the elements
+    const titleElement = await screen.findByLabelText('title');
+    expect(titleElement).toHaveValue(dataValueTopic1.data.title);
+    const editorElement = await screen.findByLabelText('mock-editor');
+    expect(editorElement).toHaveValue(dataValueTopic1.data.text);
+
+    // Update data
+    fireEvent.change(titleElement, {target: {value: 'new title 1'}});
+    expect(titleElement).toHaveValue('new title 1');
+    fireEvent.change(editorElement, {target: {value: 'new content 1'}});
+    expect(editorElement).toHaveValue('new content 1');
+
+    // Click on submit button
+    const submitButton = await screen.findByRole('button', {
+      name: 'submit',
+    });
+    await waitFor(async () => submitButton.click());
+
+    // Check API call
+    await waitFor(() =>
+      expect(putRequestObject).toEqual(
+        expect.objectContaining({
+          method: 'PUT',
+          url: 'http://localhost/incredy/api/v1/topic/1',
+        }),
+      ),
+    );
+
+    await waitFor(() =>
+      expect(putRequestData).toEqual(
+        expect.objectContaining({
+          title: 'new title 1',
+          text: 'new content 1',
+        }),
+      ),
+    );
+  });
+
+  it('should be able to cancel changes in add mode', async () => {
+    useParams.mockReturnValue({topicId: 'add'});
+
+    let postCalled: boolean = false;
+    const myPostHandler = http.post(
+      APP_CONFIG.BACKEND_PREFIX + Api.Topic.create(),
+      async ({request, params}) => {
+        postCalled = true;
+        return HttpResponse.json({});
+      },
+    );
+    server.use(myPostHandler);
+
+    const router = createMemoryRouter([{path: '*', element: <EditTopic />}]);
+    render(<RouterProvider router={router} />);
+
+    // Gets the elements
+    const titleElement = await screen.findByLabelText('title');
+    expect(titleElement).toHaveValue('');
+    const editorElement = await screen.findByLabelText('mock-editor');
+    expect(editorElement).toHaveValue('');
+
+    // Set data
+    fireEvent.change(titleElement, {target: {value: 'new title 1'}});
+    expect(titleElement).toHaveValue('new title 1');
+    fireEvent.change(editorElement, {target: {value: 'new content 1'}});
+    expect(editorElement).toHaveValue('new content 1');
+
+    // Click on cancel button
+    const cancelButton = await waitFor(async () =>
+      screen.findByRole('button', {
+        name: 'cancel',
+      }),
+    );
+    await waitFor(async () => cancelButton.click());
+
+    // Checks that the form is resetted
+    expect(titleElement).toHaveValue('');
+    expect(editorElement).toHaveValue('');
+
+    // Check API call
+    await waitFor(() => expect(postCalled).toEqual(false));
+  });
+
+  it('should be able to cancel changes in edit mode', async () => {
+    useParams.mockReturnValue({topicId: '1'});
+
+    const myGetHandler = http.get(
+      APP_CONFIG.BACKEND_PREFIX + Api.Topic.get(1),
+      () => {
+        return HttpResponse.json(dataValueTopic1);
+      },
+    );
+    server.use(myGetHandler);
+
+    let putCalled: boolean = false;
+    const myPutHandler = http.put(
+      APP_CONFIG.BACKEND_PREFIX + Api.Topic.update(1),
+      async ({request, params}) => {
+        putCalled = true;
+      },
+    );
+    server.use(myPutHandler);
+
+    const router = createMemoryRouter([{path: '*', element: <EditTopic />}]);
+    render(<RouterProvider router={router} />);
+
+    // Gets the elements
+    const titleElement = await screen.findByLabelText('title');
+    expect(titleElement).toHaveValue(dataValueTopic1.data.title);
+    const editorElement = await screen.findByLabelText('mock-editor');
+    expect(editorElement).toHaveValue(dataValueTopic1.data.text);
+
+    // Update data
+    fireEvent.change(titleElement, {target: {value: 'new title 1'}});
+    expect(titleElement).toHaveValue('new title 1');
+    fireEvent.change(editorElement, {target: {value: 'new content 1'}});
+    expect(editorElement).toHaveValue('new content 1');
+
+    // Click on cancel button
+    const cancelButton = await screen.findByRole('button', {
+      name: 'cancel',
+    });
+    await waitFor(async () => cancelButton.click());
+
+    // Check API call
+    await waitFor(() =>
+      expect(titleElement).toHaveValue(dataValueTopic1.data.title),
+    );
+    await waitFor(() =>
+      expect(editorElement).toHaveValue(dataValueTopic1.data.text),
+    );
+
+    await waitFor(() => expect(putCalled).toEqual(false));
   });
 });
