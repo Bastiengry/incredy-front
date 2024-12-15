1f5afef5ac1619145a4adaf6ec7941f6b226073b
bgry <bgryspee@gmail.com>
2024-12-15 21:10:12 +0100

    Add unit test for App Component

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,22 @@
 import {render} from '@testing-library/react';
 import App from '../App';
 
+jest.mock('../security', () => ({
+  keycloak: {
+    init: jest.fn().mockImplementation(() => ({
+      catch: jest.fn(),
+    })),
+  },
+}));
+
+jest.mock('../pages', () => ({
+  HomePage: () => <div>HOME</div>,
+  EditTopic: () => <div>EDIT_TOPIC</div>,
+  ViewTopic: () => <div>VIEW_TOPIC</div>,
+}));
+
 describe('The application', () => {
   it('renders well', async () => {
-    // render(<App />);
+    render(<App />);
   });
 });
